75949f922fc92d45bc77e21d818d79c74ffb3c3d
Kishan Patel <kbpatel309@gmail.com>
2026-04-11 22:50:52 -0500

    collect long words in article in list

--- parser.py
+++ parser.py
@@ -1,11 +1,17 @@
-# THE MISSION: Just open and read the first line.
-
 try:
-    with open('messy_data.txt', 'r') as file:
-        first_line = file.readline()
-        print("--- MISSION SUCCESS: FIRST LINE DETECTED ---")
-        print(first_line)
-except FileNotFoundError:
-    print("ERROR: The file 'messy_data.txt' wasn't found in this folder.")
+    with open("messy_data.txt", 'r') as file:
+        content = file.read() # Read the whole thing
+        words = content.split() # Turn the text into a list of words
 
-    
+        long_words = [] # This is your empty storage container
+
+        for word in words:
+            # THE SCALPEL: Strip away common punctation from the edges
+            clean_word = word.strip(".,!?\"'")
+            if len(clean_word) > 7:
+                long_words.append(clean_word)
+
+        print(f"--- INVESTIGATION COMPLETE: {len(long_words)} COMPLEX WORDS FOUND")
+        print(long_words)
+except FileNotFoundError:
+    print("Error: File not found.")
