a5b505822225d72d9cc29a3d811c555233bfe528
Kishan Patel <kbpatel309@gmail.com>
2026-04-13 22:22:12 -0500

    added dictionary

--- parser.py
+++ parser.py
@@ -3,15 +3,26 @@ try:
         content = file.read() # Read the whole thing
         words = content.split() # Turn the text into a list of words
 
-        long_words = [] # This is your empty storage container
+        # 1. THE ANALYSIS ENGINE (The Dictionary)
+        word_counts = {}
 
         for word in words:
-            # THE SCALPEL: Strip away common punctation from the edges
-            clean_word = word.strip(".,!?\"'")
-            if len(clean_word) > 7:
-                long_words.append(clean_word)
+            # 2. THE SCALPEL: Strip away common punctation from the edges
+            clean_word = word.strip(".,!?\"'").lower()
 
-        print(f"--- INVESTIGATION COMPLETE: {len(long_words)} COMPLEX WORDS FOUND")
-        print(long_words)
+            if len(clean_word) > 7:
+                # 3. THE TALLY LOGIC
+                if clean_word in word_counts:
+                    # We've seen it before, add 1
+                    word_counts[clean_word] = word_counts[clean_word] + 1
+                else:
+                    # First time seeing it, start the tally at 1
+                    word_counts[clean_word] = 1
+
+        # 4. PRINTING THE REPORT
+        print(f"--- FREQUENCY ANALYSIS REPORT" )
+        for found_word, tally in word_counts.items():
+            print(f"{found_word}: {tally}")
+            
 except FileNotFoundError:
     print("Error: File not found.")
