7a9c13100b550f58553260ce6954aef7a730e590
Kishan Patel <kbpatel309@gmail.com>
2026-04-17 23:39:50 -0500

    added two filters to test parser

--- test_parser.py
+++ test_parser.py
@@ -3,5 +3,19 @@ try:
     with open('iraq_data.txt', 'r') as file:
         content = file.read()
         print(content)
+
+        words = {}
+
+        for word in words:
+            clean_word = word.strip(".,!?\"'").lower()
+
+            # FILTER 1: High-complexity themes
+            if len(clean_word) > 8:
+                # Tally as a theme...
+
+            # FILTER 2: Potential Years
+            if len(clean_word) == 4 and clean_word.isdigit():
+                # Tally as a date...
+
 except FileNotFoundError:
     print("File not found.")
